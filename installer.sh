c1619d9ee8a1b1eeab1a055b142e82ad868b3227
Kerr <mxwkerr@gmail.com>
2021-04-20 19:00:49 +0100

    Up to network conf

--- installer.sh
+++ installer.sh
@@ -14,10 +14,29 @@ echo 1 # Partition number
 echo   # First sector (Accept default: 1)
 echo   # Last sector (Accept default: varies)
 echo w # Write changes
-) | fdisk
+) | fdisk /dev/sda
 
 mkfs.ext4 /dev/sda1
 mount /dev/sda1 /mnt
 
 
 pacstrap /mnt base linux linux-firmware
+
+
+# Configuration
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+
+# GB time
+ls -sf /usr/share/zoneinfo/GB/GB /etc/localtime
+hwclock --systohc
+locale-gen
+echo "LANG=en_GB.UFT-8" > /etc/locale.conf
+echo "/usr/share/kbd/keymaps/i386/qwerty/uk.map.gz" > /etc/vconsole.conf
+
+# Networking Conf
+read hstnm
+echo $hstnm > /etc/hostname
+echo "127.0.0.1	localhost"
+echo "::1		localhost"
+echo "127.0.1.1	$myhostname.localdomain	$myhostname"
