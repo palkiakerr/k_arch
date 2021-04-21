005919d984255be6947b89f9ef25ec03df8270ca
Kerr <mxwkerr@gmail.com>
2021-04-21 12:06:33 +0100

    no

--- installer.sh
+++ installer.sh
@@ -7,17 +7,11 @@ timedatectl set-ntp true
 
 # Partitioning for DOS sys, code ripped from stackoverflow
 (
-#echo o # Create a new empty DOS partition table
-#echo n
-#echo p
-#echo 1
-
 echo n # Add a new partition
 echo p # Primary partition
 echo 2 # Partition number
 echo   # First sector (Accept default: 1)
 echo   # Last sector (Accept default: varies)
-
 echo w # Write changes
 ) | fdisk /dev/sda
 
