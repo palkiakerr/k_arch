eff192a09cea0e5d882c3bfd746482ce544fa978
palkiakerr <mxwkerr@gmail.com>
2021-04-28 12:16:39 +0000

    no

--- vmanage.sh
+++ vmanage.sh
@@ -22,19 +22,20 @@ kali="https://cdimage.kali.org/kali-2021.1/kali-linux-2021.1-live-amd64.iso"
 
 
 # Sets up directory structure
-if [ $1 == "setup" ]
+if [ "$1" == "setup" ]
 then
     echo "Make sure you have set VMPATH, else default to ~/virtual"
     mkdir -p $VMPATH $VMPATH/iso/default $VMPATH/machines
+    exit 0
 fi
 
 
 # Download commonly used ISOs, download addressed defined manually above
 # To-DO, pull ISOs from a .config, add auto key verification?
-if [ $1 == "download" ]
+if [ "$1" == "download" ]
 then
 
-    if [ $2 == "all" ]
+    if [ "$2" == "all" ]
     then
         rm ~/documents/iso/default/*.iso
         wget $archlinux $debian $qubes $manjaro $kali
@@ -42,35 +43,35 @@ then
         ls $VMPATH/iso/default/*.iso
     fi
 
-    if [ $2 == "archlinux" ]
+    if [ "$2" == "archlinux" ]
     then
         echo "Refreshing ISO for $2"
         ls $VMPATH/iso/default/archlinux*.iso
         wget $archlinux
     fi
 
-    if [ $2 == "debian" ]
+    if [ "$2" == "debian" ]
     then
         echo "Refreshing ISO for $2"
         ls $VMPATH/iso/default/debian*.iso
         wget $debian
     fi
 
-    if [ $2 == "qubes" ]
+    if [ "$2" == "qubes" ]
     then
         echo "Refreshing ISO for $2"
         ls $VMPATH/iso/default/qubes*.iso
         wget $qubes
     fi
 
-    if [ $2 == "kali" ]
+    if [ "$2" == "kali" ]
     then
         echo "Refreshing ISO for $2"
         ls $VMPATH/iso/default/kali*.iso
         wget $kali
     fi
 
-    if [ $2 == "manjaro" ]
+    if [ "$2" == "manjaro" ]
     then
         echo "Refreshing ISO for $2"
         ls $VMPATH/iso/default/manjaro*.iso
@@ -81,14 +82,14 @@ fi
 
 # Comands for creating a "fresh" vm with the default 2 GB ram and 15 Gb hdd
 # first checks if the ISO exists, always deletes the previous default fresh vm
-if [ $1 == "fresh" && -n $(ls $VMPATH/iso/default/$2*.iso) ]
+if [ $1 == "fresh" ] #&& -n "$(ls $VMPATH/iso/default/$2*.iso)" ]
 then
-    VboxManage unregistervm --delete "$2-default"
-    VboxManage createvm --name "$2-default" --basefolder $VMPATH/machines --ostype ArchLinux_64 --default --register
-    VboxManage "$2-default" --memory 2048 --cpus  1
-    VboxManage modifyhd $VMPATH/machines/$2-default/$2-default.vdi --resize 15000
-    VboxManage storageattach $2-default --storagectl IDE --port 1 --device 0 --type dvddrive  --medium $($VMPATH/iso/default/$2.iso)
-    VboxManage startvm archtest
+    VBoxManage unregistervm --delete "$2-default"
+    VBoxManage createvm --name "$2-default" --basefolder $VMPATH/machines --ostype ArchLinux_64 --default --register
+    VBoxManage "$2-default" --memory 2048 --cpus  1
+    VBoxManage modifyhd $VMPATH/machines/$2-default/$2-default.vdi --resize 15000
+    VBoxManage storageattach $2-default --storagectl IDE --port 1 --device 0 --type dvddrive  --medium $($VMPATH/iso/default/$2.iso)
+    VBoxManage startvm archtest
 else
     echo "Error: Can't find ISO"
     echo "Debug: ISO search for $2 returns $(ls $VMPATH/iso/default/$2*.iso)"
